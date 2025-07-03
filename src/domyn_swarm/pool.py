5b319c87a602827b086a0ce5295557f2d2b64ab6
Federico D'Ambrosio <federico.dambrosio@igenius.ai>
2025-07-03 14:34:39 +0200

    feat: add swarm pool data models

---
+++ src/domyn_swarm/pool.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+from domyn_swarm import DomynLLMSwarm
+
+class SwarmPoolElement(BaseModel):
+    name: str
+    config_path: str
+
+class SwarmPoolConfig(BaseModel):
+    pool: list[SwarmPoolElement]
+
+class SwarmPool(BaseModel):
+    swarms: list[DomynLLMSwarm]
