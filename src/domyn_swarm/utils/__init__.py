b43e415a995c430f2155f4fa27d464b900124a1f
Francesco Bertolotti <francesco.bertolotti@igenius.ai>
2025-07-07 10:37:14 +0200

    CamelCase -> snake_case update

--- src/domyn_swarm/utils/__init__.py
+++ src/domyn_swarm/utils/__init__.py
@@ -1,2 +1,2 @@
-from domyn_swarm.utils.EnvPath import EnvPath
-from domyn_swarm.utils.ClickEnvPath import ClickEnvPath
+from domyn_swarm.utils.env_path import EnvPath
+from domyn_swarm.utils.click_env_path import ClickEnvPath
