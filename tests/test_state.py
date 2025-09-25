97ea19c0d7dd76cb49af3251a011c314775bfa68
arognoni-domyn <alessandro.rognoni@domyn.com>
2025-09-25 13:59:13 +0200

    fix(tests): fix state tests and add new ones

--- tests/test_state.py
+++ tests/test_state.py
@@ -1,6 +1,7 @@
 """Test the state persistence."""
 
 import sqlite3
+from datetime import datetime
 from pathlib import Path
 
 import pytest
@@ -23,18 +24,22 @@ class TestSwarmStateManager:
     """Test the tate persistence."""
 
     @pytest.fixture
-    def swarm(self, tmp_path: Path) -> DomynLLMSwarm:
+    def swarm(self, tmp_path: Path, monkeypatch: pytest.MonkeyPatch) -> DomynLLMSwarm:
         """Swarm.
-
-        Args:
-            tmp_path (Path): Temporary path to store the DB.
 
         Returns:
             DomynLLMSwarm: Swarm.
         """
+
+        def mock_db_path(_x=None) -> Path:
+            return tmp_path / SwarmStateManager.DB_NAME
+
+        # db_path = tmp_path / SwarmStateManager.DB_NAME
+        monkeypatch.setattr(SwarmStateManager, "_get_db_path", mock_db_path)
         return DomynLLMSwarm(
             name="swarm",
             cfg=DomynLLMSwarmConfig(
+                name="fake",
                 hf_home=".",
                 model="Qwen/Qwen3-32B",
                 revision=None,
@@ -66,6 +71,7 @@ class TestSwarmStateManager:
                 meta=dict(
                     lb_jobid=1234,
                     jobid=5678,
+                    lb_node="lrdn4759",
                     port=9003,
                     name="swarm",
                 ),
@@ -84,7 +90,12 @@ class TestSwarmStateManager:
         Returns:
             SwarmStateManager: State manager.
         """
-        return SwarmStateManager(swarm=swarm)
+        return swarm._state_mgr
+
+    def test_get_db_path(self) -> None:
+        """The DB name must be correct."""
+        db_path = SwarmStateManager._get_db_path()
+        assert db_path.name == SwarmStateManager.DB_NAME
 
     def test_create_missing_table(self, state_manager: SwarmStateManager) -> None:
         """Test the DB creation.
@@ -94,8 +105,10 @@ class TestSwarmStateManager:
         Args:
             state_manager (SwarmStateManager): State manager.
         """
+        db_path = state_manager._get_db_path()
+        db_path.unlink(missing_ok=True)
         state_manager._create_db_if_missing()
-        with sqlite3.connect(state_manager.db_path) as cnx:
+        with sqlite3.connect(db_path) as cnx:
             cursor = cnx.cursor()
             cursor.execute(
                 "SELECT name FROM sqlite_master WHERE type='table' AND name='swarm';"
@@ -115,7 +128,8 @@ class TestSwarmStateManager:
         """
         state_manager._create_db_if_missing()
         state_manager._create_db_if_missing()
-        with sqlite3.connect(state_manager.db_path) as cnx:
+        db_path = state_manager._get_db_path()
+        with sqlite3.connect(db_path) as cnx:
             cursor = cnx.cursor()
             cursor.execute(
                 "SELECT name FROM sqlite_master WHERE type='table' AND name='swarm';"
@@ -125,59 +139,28 @@ class TestSwarmStateManager:
 
         assert name[0] == "swarm"
 
-    def test_get_flat_data_dict(self, state_manager: SwarmStateManager) -> None:
-        """Check the JSON is flattened correctly.
+    def test_get_record(self, state_manager: SwarmStateManager) -> None:
+        """Check the table record.
 
         Args:
             state_manager (SwarmStateManager): State manager.
         """
-        data = state_manager._get_flat_data_dict()
-        assert data == {
-            "name": state_manager.swarm.name,
-            "jobid": state_manager.swarm.jobid,
-            "lb_jobid": state_manager.swarm.lb_jobid,
-            "lb_node": state_manager.swarm.lb_node,
-            "endpoint": state_manager.swarm.endpoint,
-            "delete_on_exit": state_manager.swarm.delete_on_exit,
-            "model": state_manager.swarm.model,
-            "driver_cpus_per_task": state_manager.swarm.cfg.driver.cpus_per_task,
-            "driver_mem": state_manager.swarm.cfg.driver.mem,
-            "driver_threads_per_core": state_manager.swarm.cfg.driver.threads_per_core,
-            "driver_wall_time": state_manager.swarm.cfg.driver.wall_time,
-            "driver_enable_proxy_buffering": state_manager.swarm.cfg.driver.enable_proxy_buffering,
-            "driver_nginx_timeout": state_manager.swarm.cfg.driver.nginx_timeout,
-            "hf_home": str(state_manager.swarm.cfg.hf_home),
-            "revision": state_manager.swarm.cfg.revision,
-            "replicas": state_manager.swarm.cfg.replicas,
-            "gpus_per_replica": state_manager.swarm.cfg.gpus_per_replica,
-            "gpus_per_node": state_manager.swarm.cfg.gpus_per_node,
-            "replicas_per_node": state_manager.swarm.cfg.replicas_per_node,
-            "nodes": state_manager.swarm.cfg.nodes,
-            "cpus_per_task": state_manager.swarm.cfg.cpus_per_task,
-            "requires_ray": state_manager.swarm.cfg.requires_ray,
-            "mem_per_cpu": state_manager.swarm.cfg.mem_per_cpu,
-            "partition": state_manager.swarm.cfg.partition,
-            "account": state_manager.swarm.cfg.account,
-            "vllm_image": str(state_manager.swarm.cfg.vllm_image),
-            "nginx_image": str(state_manager.swarm.cfg.nginx_image),
-            "lb_wait": state_manager.swarm.cfg.lb_wait,
-            "lb_port": state_manager.swarm.cfg.lb_port,
-            "home_directory": str(state_manager.swarm.cfg.home_directory),
-            "log_directory": str(state_manager.swarm.cfg.log_directory),
-            "max_concurrent_requests": state_manager.swarm.cfg.max_concurrent_requests,
-            "poll_interval": state_manager.swarm.cfg.poll_interval,
-            "template_path": str(state_manager.swarm.cfg.template_path),
-            "nginx_template_path": str(state_manager.swarm.cfg.nginx_template_path),
-            "vllm_args": state_manager.swarm.cfg.vllm_args,
-            "vllm_port": state_manager.swarm.cfg.vllm_port,
-            "ray_port": state_manager.swarm.cfg.ray_port,
-            "ray_dashboard_port": state_manager.swarm.cfg.ray_dashboard_port,
-            "venv_path": state_manager.swarm.cfg.venv_path,
-            "time_limit": state_manager.swarm.cfg.time_limit,
-            "exclude_nodes": state_manager.swarm.cfg.exclude_nodes,
-            "node_list": state_manager.swarm.cfg.node_list,
-            "mail_user": state_manager.swarm.cfg.mail_user,
-        }
+        data = state_manager._get_record("fake")
+        assert data["deployment_name"] == "fake"
+        assert isinstance(data["swarm"], str)
+        assert isinstance(data["cfg"], str)
+        assert isinstance(data["serving_handle"], str)
+        assert isinstance(data["creation_dt"], datetime)
+
+    def test_get_record_null_handle(self, state_manager: SwarmStateManager) -> None:
+        """The handle must be valid.
+
+        Args:
+            state_manager (SwarmStateManager): State manager.
+        """
+        state_manager.swarm.serving_handle = None
+        with pytest.raises(ValueError):
+            state_manager._get_record("fake")
 
     def test_save(self, state_manager: SwarmStateManager) -> None:
         """Check the record is saved in the DB.
@@ -186,58 +169,65 @@ class TestSwarmStateManager:
             state_manager (SwarmStateManager): State manager.
         """
         state_manager._create_db_if_missing()
-        state_manager.save()
-        with sqlite3.connect(state_manager.db_path) as cnx:
+        state_manager.save(deployment_name="name")
+        db_path = state_manager._get_db_path()
+        with sqlite3.connect(db_path) as cnx:
             cursor = cnx.cursor()
-            cursor.execute(
-                f"SELECT * FROM swarm WHERE jobid = {state_manager.swarm.jobid};"
-            )
+            cursor.execute("SELECT * FROM swarm WHERE deployment_name='name';")
             row = cursor.fetchall()
 
         assert len(row) == 1
 
-    def test_update_lb_data(self, state_manager: SwarmStateManager) -> None:
-        """Test LB data is updated correctly.
-
-        Args:
-            state_manager (SwarmStateManager): State manager.
-        """
-        state_manager._create_db_if_missing()
-        state_manager.save()
-        state_manager.swarm.cfg.lb_port = 7777
-        state_manager.swarm.lb_node = "node2"
-        state_manager.swarm.endpoint = "http://updated-lb:7777"
-
-        state_manager.update_lb_data()
-
-        with sqlite3.connect(state_manager.db_path) as cnx:
-            cursor = cnx.cursor()
-            cursor.execute(
-                "SELECT lb_port, lb_node, endpoint FROM swarm"
-                f" WHERE jobid = {state_manager.swarm.jobid};"
-            )
-            lb_port, lb_node, endpoint = cursor.fetchone()
-
-        assert lb_port == state_manager.swarm.cfg.lb_port
-        assert lb_node == state_manager.swarm.lb_node
-        assert endpoint == state_manager.swarm.endpoint
-
-    def test_load(self, state_manager: SwarmStateManager) -> None:
+    def test_load_slurm(self, state_manager: SwarmStateManager) -> None:
         """Test the swarm initialization from a record.
 
         Args:
             state_manager (SwarmStateManager): State manager.
         """
         state_manager._create_db_if_missing()
-        state_manager.save()
+        state_manager.save(deployment_name="fake")
 
-        swarm = SwarmStateManager.load(
-            state_manager.swarm.jobid,
-            home_directory=state_manager.swarm.cfg.home_directory,
-        )
+        swarm = SwarmStateManager.load(deployment_name="fake")
+        assert isinstance(swarm, DomynLLMSwarm)
 
-        assert swarm.jobid == state_manager.swarm.jobid
-        assert swarm.name == state_manager.swarm.name
+    def test_load_null_jobid(self, state_manager: SwarmStateManager) -> None:
+        """Null Job ID.
+
+        Args:
+            state_manager (SwarmStateManager): State manager.
+        """
+        state_manager.swarm.serving_handle.meta["jobid"] = None
+        state_manager._create_db_if_missing()
+        state_manager.save(deployment_name="fake")
+
+        with pytest.raises(ValueError, match="job IDs"):
+            SwarmStateManager.load(deployment_name="fake")
+
+    def test_load_null_lb_jobid(self, state_manager: SwarmStateManager) -> None:
+        """Null LB Job ID.
+
+        Args:
+            state_manager (SwarmStateManager): State manager.
+        """
+        state_manager.swarm.serving_handle.meta["lb_jobid"] = None
+        state_manager._create_db_if_missing()
+        state_manager.save(deployment_name="fake")
+
+        with pytest.raises(ValueError, match="LB job info"):
+            SwarmStateManager.load(deployment_name="fake")
+
+    def test_load_null_lb_node(self, state_manager: SwarmStateManager) -> None:
+        """Null LB Node.
+
+        Args:
+            state_manager (SwarmStateManager): State manager.
+        """
+        state_manager.swarm.serving_handle.meta["lb_node"] = None
+        state_manager._create_db_if_missing()
+        state_manager.save(deployment_name="fake")
+
+        with pytest.raises(ValueError, match="LB job info"):
+            SwarmStateManager.load(deployment_name="fake")
 
     def test_load_unknown_job(self, state_manager: SwarmStateManager) -> None:
         """Exception is raised if the swarm is not found in the DB.
@@ -245,11 +235,8 @@ class TestSwarmStateManager:
         Args:
             state_manager (SwarmStateManager): State manager.
         """
-        with pytest.raises(JobNotFoundError):
-            SwarmStateManager.load(
-                state_manager.swarm.jobid,
-                home_directory=state_manager.swarm.cfg.home_directory,
-            )
+        with pytest.raises(JobNotFoundError, match="fake"):
+            SwarmStateManager.load(deployment_name="fake")
 
     def test_delete_record(self, state_manager: SwarmStateManager) -> None:
         """Test the record deletion from the DB.
@@ -258,14 +245,13 @@ class TestSwarmStateManager:
             state_manager (SwarmStateManager): State manager.
         """
         state_manager._create_db_if_missing()
-        state_manager.save()
-        state_manager.swarm.delete_record()
+        state_manager.save(deployment_name="fake")
+        state_manager.swarm.delete_record(deployment_name="fake")
+        db_path = state_manager._get_db_path()
 
-        with sqlite3.connect(state_manager.db_path) as cnx:
+        with sqlite3.connect(db_path) as cnx:
             cursor = cnx.cursor()
-            cursor.execute(
-                f"SELECT * FROM swarm WHERE jobid = {state_manager.swarm.jobid};"
-            )
+            cursor.execute("SELECT * FROM swarm WHERE deployment_name='fake';")
             rows = cursor.fetchall()
 
         assert not rows
