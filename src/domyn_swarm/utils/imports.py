f0b7278f6cc30b2d952c504a8cee21d5af00c943
Federico D'Ambrosio <federico.dambrosio@igenius.ai>
2025-09-26 12:56:46 +0200

    ci: add step to check if leptonai is actually installed

--- src/domyn_swarm/utils/imports.py
+++ src/domyn_swarm/utils/imports.py
@@ -3,7 +3,8 @@ try:
 
     _HAVE_LEPTON = True
     _ = _LeptonAPIClient()
-except Exception:
+except Exception as e:  # pylint: disable=broad-except
+    print(f"Lepton import failed: {e}")
     _LeptonAPIClient = None  # type: ignore
     _HAVE_LEPTON = False
 
