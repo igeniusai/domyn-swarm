1a012c6089b27b2fd953bc57e94887847e2eac1d
Federico D'Ambrosio <federico.dambrosio@igenius.ai>
2025-10-03 14:35:54 +0200

    style: fix end of files

--- tests/cli/utils.py
+++ tests/cli/utils.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 def assert_cli_ok(result, msg="CLI command failed unexpectedly"):
     """Assert a CLI result was successful."""
     assert result.exit_code == 0, (
